66be35bc982d09c2ff99ca55bbc88fdd3c424ebc
Alice <alice.jost@outlook.de>
2021-11-12 10:58:29 +0100

    create auth routes

--- models/Call.model.js
+++ models/Call.model.js
@@ -8,7 +8,7 @@ const callSchema = new Schema(
         },
         toWhom: {
             type: Schema.Types.ObjectId,
-            ref: 'User'
+            ref: 'User' //TODO
         },
         time: {
             type: Number,
