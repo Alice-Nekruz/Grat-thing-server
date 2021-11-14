c0cbbbcc3077967258e33c806ae47169d4fee348
Nekruz Avgani <nekruz.avgani@gmail.com>
2021-11-14 22:40:58 +0000

    CORS from 3014 to 3001

--- config/index.js
+++ config/index.js
@@ -35,7 +35,7 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3014",
+      origin: process.env.ORIGIN || "http://localhost:3001",
     })
   );
 
